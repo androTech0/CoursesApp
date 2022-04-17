50e0789685a55081bb746af56739cecf037298ac
androTech0 <khbu2017@gmail.com>
2022-04-17 14:57:45 +0300

    test

--- app/src/main/java/com/helmy/coursesapp/LecturerMainActivity.kt
+++ app/src/main/java/com/helmy/coursesapp/LecturerMainActivity.kt
@@ -3,14 +3,9 @@ package com.helmy.coursesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
-
-
+class LecturerMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_lecturer_main)
     }
 }
---
+++ app/src/main/java/com/helmy/coursesapp/StudentMainActivity.kt
@@ -0,0 +1,16 @@
+package com.helmy.coursesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentMainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
