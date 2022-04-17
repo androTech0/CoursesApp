9b5777659f83bd44f929650e6b020afde7999794
androTech0 <khbu2017@gmail.com>
2022-04-17 18:08:13 +0300

    test

--- app/src/main/java/com/helmy/coursesapp/LecturerMainActivity.kt
+++ app/src/main/java/com/helmy/coursesapp/LecturerMainActivity.kt
@@ -2,10 +2,43 @@ package com.helmy.coursesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.helmy.coursesapp.LecturerMainFragments.AddFragment
+import com.helmy.coursesapp.LecturerMainFragments.CoursesFragment
+import com.helmy.coursesapp.LecturerMainFragments.EditFragment
+import com.helmy.coursesapp.LecturerMainFragments.LecturerChattingFragment
+import kotlinx.android.synthetic.main.activity_lecturer_main.*
 
 class LecturerMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lecturer_main)
+
+        replaceFragment(CoursesFragment())
+
+        bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.Courses ->{
+                    replaceFragment(CoursesFragment())
+                }
+                R.id.Add ->{
+                    replaceFragment(AddFragment())
+                }
+                R.id.Edit ->{
+                    replaceFragment(EditFragment())
+                }
+                R.id.Chat ->{
+                    replaceFragment(LecturerChattingFragment())
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment:Fragment){
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout,fragment)
+        transaction.commit()
     }
 }
