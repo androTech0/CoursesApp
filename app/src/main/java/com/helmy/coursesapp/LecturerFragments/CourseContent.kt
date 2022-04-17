caef699aa876c0d911f51838175f0f37782a6117
androTech0 <khbu2017@gmail.com>
2022-04-17 22:23:10 +0300

    7

--- app/src/main/java/com/helmy/coursesapp/LecturerFragments/CourseContent.kt
+++ app/src/main/java/com/helmy/coursesapp/LecturerFragments/CourseContent.kt
@@ -2,14 +2,25 @@ package com.helmy.coursesapp.LecturerFragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.helmy.coursesapp.R
 
 class CourseContent : AppCompatActivity() {
+
+    private val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_content)
 
-        val courseId = intent.getStringExtra("CourseId").toString()
+        val CourseId = intent.getStringExtra("CourseId").toString()
+
+        db.collection("Courses").whereEqualTo("CourseId",CourseId).get().addOnSuccessListener {
+
+        }
+
+
 
     }
 }
